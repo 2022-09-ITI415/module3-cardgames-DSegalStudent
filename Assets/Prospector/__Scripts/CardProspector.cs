7160dc8d813f9e9953325accc08f3ac82cd7a089
DSegalStudent <dan.segal359@gmail.com>
2022-10-31 17:42:00 -0400

    Added the Card Prospector Script
    Converts Cards to Card Prospector Cards

--- Assets/Prospector/__Scripts/CardProspector.cs
+++ Assets/Prospector/__Scripts/CardProspector.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardProspector : MonoBehaviour
+//An enum defines a variable type with a few prenamed values //a
+public enum eCardState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    drawpile,
+    tableau,
+    target,
+    discard
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+public class CardProspector : Card
+{
+    [Header("Set Dynamically: CardProspector")]
+    //This is how you ise the enum eCardState
+    public eCardState state = eCardState.drawpile;
+    //The hiddenBy list stores which other cards will keep this one face down
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    // the LayoutID matches this card to the tableau XML if its a tableau card
+    public int layoutID;
+    //the SlotDef class stores infromation pulled in from the LayoutXML <slot>
+    public SlotDef slotDef;
+    // Start is called before the first frame update
+
 }
