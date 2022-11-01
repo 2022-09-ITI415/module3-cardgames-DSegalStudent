eed6abb07465df9dd64f32c2618241cc6b393e9b
DSegalStudent <dan.segal359@gmail.com>
2022-11-01 19:16:28 -0400

    created a prefab for the floating score

---
+++ Assets/Prospector/__Scripts/Scoreboard.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//The Scoreboard class manages showing the score to the player
+public class Scoreboard : MonoBehaviour
+
+{
+    public static Scoreboard S; // The singleton for Scoreboard
+
+    [Header("Set in Inspector")]
+    public GameObject prefabFloatingScore;
+
+    [Header("Set Dynamically")]
+    [SerializeField] private int _score = 0;
+    [SerializeField] private string _scoreString;
+
+    private Transform canvasTrans;
+
+    //the score properly also sets the scoreString
+    public int score
+    {
+        get
+        {
+            return (_score);
+        }
+        set
+        {
+            _score = value;
+            _scoreString = _score.ToString("NO");
+        }
+    }
+
+    //The scoreString property also sets the Text.text
+    public string scoreString
+    {
+        get
+        {
+            return (_scoreString);
+        }
+        set
+        {
+            _scoreString = value;
+            GetComponent<Text>().text = _scoreString;
+        }
+    }
+
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this; // set the prvate singleton
+        }
+        else
+        {
+            Debug.LogError("ERROR: Scoreboard.Awake(): S is already set!");
+        }
+        canvasTrans = transform.parent;
+    }
+
+    //When called by SendMessage, this adds the fs.score to this.score
+    public void FSCallback(FloatingScore fs)
+    {
+        score += fs.score;
+    }
+
+    //This will instantial a new Floating score GameObject and initialize it
+    //It also returns a pointer to the Floating Score craeted so that the
+    //Calling function can do more with it (Set font sizes ...)
+
+    public FloatingScore CreateFloatingScore(int amt, List<Vector2> pts)
+    {
+        GameObject go = Instantiate<GameObject>(prefabFloatingScore);
+        go.transform.SetParent(canvasTrans);
+        FloatingScore fs = go.GetComponent<FloatingScore>();
+        fs.score = amt;
+        fs.reportFinishTo = this.gameObject; //Set fs to call back to this
+        fs.Init(pts);
+        return (fs);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
